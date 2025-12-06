0e82f6e025c71a3e2fa517d77ba4d738fabc7021
Ilya Strukov <ilya@istrukov.com>
2025-12-06 13:54:12 +0000

    I dunno

--- experiments/proto1/src/main.cpp
+++ experiments/proto1/src/main.cpp
@@ -5,55 +5,68 @@ constexpr uint8_t kLedDataPin = 3;
 constexpr size_t kNumLeds = 4;
 constexpr uint8_t kBrightness = 128;
 
+constexpr size_t kNumRows = 2;
+constexpr size_t kNumCols = 2;
+
+const uint8_t sensorPowerPins[kNumRows] = {2, 5};
+const uint8_t sensorOutPins[kNumCols] = {4, 7};
+
+constexpr int kSensorPowerDelay = 2;
+
 CRGB leds[kNumLeds];
 
 void setup() {
   FastLED.addLeds<WS2812B, kLedDataPin, GRB>(leds, kNumLeds)
       .setCorrection(TypicalLEDStrip);
-}
 
-void lightLed(size_t n) {
-  fill_solid(leds, kNumLeds, CRGB::Black);
-  leds[n] = CRGB::White;
-  FastLED.show();
-}
-
-void spin() {
-  lightLed(0);
-  delay(500);
-
-  lightLed(2);
-  delay(500);
-
-  lightLed(3);
-  delay(500);
-
-  lightLed(1);
-  delay(500);
-}
-
-void rainbow() {
-  static uint8_t hue = 0;
-  for (size_t i = 0; i < kNumLeds; ++i) {
-    leds[i] = CHSV(hue + i * 8, 255, 255);
+  for (int i = 0; i < kNumRows; i++) {
+    pinMode(sensorPowerPins[i], OUTPUT);
   }
-  FastLED.show();
-  ++hue;
-  delay(20);
+
+  for (int i = 0; i < kNumCols; i++) {
+    pinMode(sensorOutPins[i], INPUT_PULLUP);
+  }
+
+  digitalWrite(sensorPowerPins[0], HIGH);
 }
 
-void staticLight() {
-  fill_solid(leds, kNumLeds, CRGB::White);
-  FastLED.show();
-  delay(2000);
+int ledNumber(int row, int col) {
+  if (row == 0 && col == 0) {
+    return 0;
+  } else if (row == 0 && col == 1) {
+    return 1;
+  } else if (row == 1 && col == 1) {
+    return 3;
+  } else {
+    return 2;
+  }
+}
+
+void setLed(int row, int col, bool on) {
+  int n = ledNumber(row, col);
+  if (on) {
+    leds[n] = CRGB::White;
+  } else {
+    leds[n] = CRGB::Black;
+  }
 }
 
 void loop() {
-  spin();
+  fill_solid(leds, kNumLeds, CRGB::Black);
 
-  for (int i = 0; i < 200; i++) {
-    rainbow();
+  for (int row = 0; row < kNumRows; row++) {
+    digitalWrite(sensorPowerPins[row], HIGH);
+    delay(kSensorPowerDelay);
+
+    for (int col = 0; col < kNumCols; col++) {
+      int sensorValue = digitalRead(sensorOutPins[col]);
+      bool magnetPresent = sensorValue == LOW;
+      setLed(row, col, magnetPresent);
+    }
+
+    digitalWrite(sensorPowerPins[row], LOW);
+    delay(kSensorPowerDelay);
   }
 
-  staticLight();
+  FastLED.show();
 }
